3a98354fd978840d53fe3504bdf5cfdc34c615a4
dsreecharan <d.sreecharan999@gmail.com>
2026-03-03 15:26:56 +0530

    0-F

--- src/main/java/com/anurag/docvault/DocVaultApplication.java
+++ src/main/java/com/anurag/docvault/DocVaultApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocvaultApplication {
+public class DocVaultApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocvaultApplication.class, args);
     }
